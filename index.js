7ed3689927964130bd638998808924cf6c9ec5af
Talytta Carvalho <talytta110@hotmail.com>
2018-09-05 14:59:38 -0300

    refatoração após feedback

--- index.js
+++ index.js
@@ -2,36 +2,29 @@ const roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV",
 const nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
 function romanToInt(algRoman) {
-  try {
-    isValidAlgRoman(algRoman);
-    algRoman = algRoman.toUpperCase();
-    let result = 0;
-    for (let num in nums) {
-      while (algRoman.indexOf(roman[num]) === 0) {
-        result += nums[num];
-        algRoman = algRoman.replace(roman[num], "");
-      }
+  isValidAlgRoman(algRoman);
+  algRoman = algRoman.toUpperCase();
+  let result = 0;
+  for (let num in nums) {
+    while (algRoman.indexOf(roman[num]) === 0) {
+      result += nums[num];
+      algRoman = algRoman.replace(roman[num], "");
     }
-    return result;
-  } catch (error) {
-    return error.message;
   }
+  return result;
 }
 
 function intToRoman(num) {
-  try {
-    isValidNum(num);
-    let result = "";
-    for (let i in nums) {
-      while (num % nums[i] < num) {
-        result += roman[i];
-        num -= nums[i];
-      }
+  isValidNum(num);
+  let result = "";
+  for (let i in nums) {
+    while (num % nums[i] < num) {
+      result += roman[i];
+      num -= nums[i];
     }
-    return result;
-  } catch (error) {
-    return error.message;
   }
+  return result;
+
 }
 
 function isValidAlgRoman(algRoman) {
@@ -47,7 +40,7 @@ function isValidNum(num) {
     throw new Error("Digite o número que deseja converter.");
   } else if (typeof num !== "number") {
     throw new Error("Digite o número que deseja converter.");
-}
+  }
 }
 
 module.exports.intToRoman = intToRoman;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,49 +1,70 @@
 const assert = require("assert");
+const chai = require("chai");
+const expect = chai.expect;
+
 const lib = require("../index.js");
 const romanToInt = lib.romanToInt;
 const intToRoman = lib.intToRoman;
 
 
-describe("romanToInt()", function() {
-  it("Deve retornar o número 14.", () => {
-    assert.equal(romanToInt("XIV"), 14);
+describe("romanToInt()", function () {
+  describe("Passado um número romano, retorna um número arábico", function () {
+    it("Deve retornar o número 14.", () => {
+      assert.equal(romanToInt("XIV"), 14);
+    });
   });
 
-  it("Deve retornar o número 22.", () => {
-    assert.equal(romanToInt("xxii"), 22);
+  describe("Passado um número romano, retorna um número arábico", function () {
+    it("Deve retornar o número 22.", () => {
+      assert.equal(romanToInt("xxii"), 22);
+    });
   });
 
-  it("Deve retornar o número 22.", () => {
-    assert.equal(romanToInt("ml"), 1050);
+  describe("Passado um número romano, retorna um número arábico", function () {
+    it("Deve retornar o número 1050.", () => {
+      assert.equal(romanToInt("ml"), 1050);
+    });
   });
 
-  it('Deve retornar erro "Digite o algarismo romano que deseja converter."', () => {
-    assert.equal(romanToInt(), "Digite o algarismo romano que deseja converter.");
+  describe("Quando não hover parâmetro", function () {
+    it('Deve retornar erro "Digite o algarismo romano que deseja converter."', () => {
+      let emptyPar = () => {lib.romanToInt()};
+      expect(emptyPar).to.throw("Digite o algarismo romano que deseja converter.");
+    });
   });
 
-  it('Deve retornar erro "Digite o algarismo romano que deseja converter."', () => {
-    assert.equal(romanToInt(25), "Digite o algarismo romano que deseja converter.");
+  describe("Quando passar parâmetro incorreto", function () {
+    it('Deve retornar erro "Digite o algarismo romano que deseja converter."', () => {
+      let errorPar = () => {lib.romanToInt(25)};
+      expect(errorPar).to.throw("Digite o algarismo romano que deseja converter.");
+    });
   });
 });
 
-describe("intToRoman()", function() {
-  it('Deve retornar a string "XIV"', () => {
-    assert.equal(intToRoman(14), "XIV");
+describe("intToRoman()", function () {
+  describe("Passado um número arábico, retorna um número romano", function () {
+    it('Deve retornar a string "XIV"', () => {
+      assert.equal(intToRoman(14), "XIV");
+    });
   });
 
-  it('Deve retornar a string "MMLXIII"', () => {
-    assert.equal(intToRoman(2063), "MMLXIII");
+  describe("Passado um número arábico, retorna um número romano", function () {
+    it('Deve retornar a string "MMLXIII"', () => {
+      assert.equal(intToRoman(2063), "MMLXIII");
+    });
   });
 
-  it('Deve retornar erro "Digite o número que deseja converter."', () => {
-    assert.equal(intToRoman(), "Digite o número que deseja converter.");
+  describe("Quando não houver parâmetro", function () {
+    it('Deve retornar erro "Digite o número que deseja converter."', () => {
+      let emptyPar = () => {lib.intToRoman()};
+      expect(emptyPar).to.throw("Digite o número que deseja converter.");
+    });
   });
 
-  it('Deve retornar erro "Digite o número que deseja converter."', () => {
-    assert.equal(intToRoman("hello"), "Digite o número que deseja converter.");
-  });
-
-  it('Deve retornar erro "Digite o número que deseja converter."', () => {
-    assert.equal(intToRoman("XXI"), "Digite o número que deseja converter.");
+  describe("Quando passar parâmetro incorreto", function () {
+    it('Deve retornar erro "Digite o número que deseja converter."', () => {
+      let errorPar = () => {lib.intToRoman("XXI")};
+      expect(errorPar).to.throw("Digite o número que deseja converter.");
+    });
   });
 });
